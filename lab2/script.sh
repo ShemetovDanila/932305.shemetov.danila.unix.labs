2f85571a247b12c92cdbac55a455c8e594574547
Danila <0illiuwuwilli0@gmail.com>
2025-10-29 09:14:57 +0000

    hotX3fix

--- lab2/script.sh
+++ lab2/script.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 S=/shared L=$S/.lock I=$(hostname|cut -c1-8) C=0
 mkdir -p $S; touch $L
+
+exec 200>"$L"
 while :; do
-	f=$(
-		flock -x 200
-		i=1
-		while [ $i -le 999 ]; do
-			f=$(printf %03d $i); p=$S/$f
-			if [ ! -e "$p" ]; then
-				C=$((C+1))
-				echo "$I $C" > "$p"
-				echo $f
-			fi
-			i=$((i+1))
-		done
-	) 200>"$L"
+	flock -x 200
+	i=1
+	f=
+	while [ $i -le 999 ]; do
+		f=$(printf %03d $i); p=$S/$f
+		if [ ! -e "$p" ]; then
+			C=$((C+1))
+			echo "$I $C" > "$p"
+			echo $f
+		fi
+		i=$((i+1))
+	done
+	flock -u 200
+
 	sleep 1
 	[ -n "$f" ] && rm -f "$S/$f"
 	sleep 1
