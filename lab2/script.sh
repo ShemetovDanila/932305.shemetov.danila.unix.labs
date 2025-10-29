d68d2bc32d743166e9efea9e244cb3c4f7e4dabf
Danila <0illiuwuwilli0@gmail.com>
2025-10-29 08:55:54 +0000

    hotX2fix

--- lab2/script.sh
+++ lab2/script.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
-S=/shared L=$S/.lock I=$(hostname|cut -c1-8) C=0 N=/tmp/n
+S=/shared L=$S/.lock I=$(hostname|cut -c1-8) C=0
 mkdir -p $S; touch $L
-trap 'rm -f $N; exit' INT TERM EXIT
 while :; do
-	(
+	f=$(
 		flock -x 200
 		i=1
 		while [ $i -le 999 ]; do
 			f=$(printf %03d $i); p=$S/$f
-			[ ! -e "$p" ] && { C=$((C+1)); echo "$I $C" >"$p"; echo $f >$N; break;}
+			if [ ! -e "$p" ]; then
+				C=$((C+1))
+				echo "$I $C" > "$p"
+				echo $f
+			fi
 			i=$((i+1))
 		done
 	) 200>"$L"
 	sleep 1
-	[ -f $N ] && { f=$(cat $N); rm -f $S/$f $N; }
+	[ -n "$f" ] && rm -f "$S/$f"
 	sleep 1
 done
