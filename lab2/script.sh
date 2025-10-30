49022c0f31edeacef38538e70c34e1f728cf2be3
Danila <0illiuwuilli0@gmail.com>
2025-10-30 20:32:11 +0000

    _

--- lab2/script.sh
+++ lab2/script.sh
@@ -2,24 +2,31 @@
 S=/shared L=$S/.lock I=$(hostname|cut -c1-8) C=0
 mkdir -p $S; touch $L
 
-trap 'rm -f "$S/$f" 2>/dev/null; exit' EXIT TERM INT
-
 while :; do
-	{
-		flock -x 200
-		i=1
-		while [ $i -le 999 ]; do
-			f=$(printf %03d $i) 
-			p=$S/$f
-			if [ ! -e "$p" ]; then
-				C=$((C+1))
-				echo "$I $C" > "$p"
-				break
-			fi
-			i=$((i+1))
-		done
-	} 200>"$L"
-	sleep 1
-	rm -f "$S/$f"
-	sleep 1
+	exec 200>"$L"
+	flock -x 200
+	
+	i=1
+	f=
+	while [ $i -le 999 ]; do
+		f=$(printf %03d $i) 
+		p=$S/$f
+		if [ ! -e "$p" ]; then
+			C=$((C+1))
+			echo "$I $C" > "$p"
+			break
+		fi
+		i=$((i+1))
+	done
+	
+	flock -u 200
+	exec 200>&-
+	
+	if [ -n "$f" ]; then
+		sleep 1
+		rm -f "$S/$f"
+		sleep 1
+	else
+		sleep 2
+	fi
 done
