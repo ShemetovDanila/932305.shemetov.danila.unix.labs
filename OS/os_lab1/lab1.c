ba252f902b5260372c6fc04842f1f80e5b6b73c3
Yakovleva-Lera <0illiuwuwilli0@gmail.com>
2025-12-05 07:54:55 +0000

    +os_lab1

---
+++ OS/os_lab1/lab1.c
@@ -0,0 +1,71 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+int ready = 0;
+volatile sig_atomic_t running = 1;
+
+void handle_sigint(int sig) {
+    running = 0;
+}
+
+void* provider(void* arg) {
+    while (running) {
+        sleep(1);
+        
+        pthread_mutex_lock(&lock);
+        
+        if (ready == 1) {
+            pthread_mutex_unlock(&lock);
+            continue;
+        }
+        
+        ready = 1;
+        printf("Provider: Event provided\n");
+        
+        pthread_cond_signal(&cond);
+        
+        pthread_mutex_unlock(&lock);
+    }
+    return NULL;
+}
+
+void* consumer(void* arg) {
+    while (running) {
+        pthread_mutex_lock(&lock);
+        
+        while (ready == 0 && running) {
+            pthread_cond_wait(&cond, &lock);
+            printf("Consumer: Awoke from wait\n");
+        }
+        
+        if (!running) {
+            pthread_mutex_unlock(&lock);
+            break;
+        }
+        
+        printf("Consumer: Event consumed\n");
+        ready = 0;
+        
+        pthread_mutex_unlock(&lock);
+    }
+    return NULL;
+}
+
+int main() {
+    signal(SIGINT, handle_sigint);
+    
+    pthread_t provider_thread, consumer_thread;
+    
+    pthread_create(&provider_thread, NULL, provider, NULL);
+    pthread_create(&consumer_thread, NULL, consumer, NULL);
+    
+    pthread_join(provider_thread, NULL);
+    pthread_join(consumer_thread, NULL);
+    
+    pthread_mutex_destroy(&lock);
+    pthread_cond_destroy(&cond);
+}
